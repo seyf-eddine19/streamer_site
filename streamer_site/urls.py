3fbf0a3afa1cb694205551c3ab45a167cf99064e
Seyf Eddine <84080839+seyf-eddine19@users.noreply.github.com>
2025-07-05 18:42:15 +0100

    Add files via upload

---
+++ streamer_site/urls.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
+
+from team_dashboard.admin import create_superuser_view
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),           # ✅ هذا هو المطلوب لتفعيل set_language
+    path('create-superuser/', create_superuser_view, name='create_superuser'),
+    path('admin/', admin.site.urls),
+    path('dashboard/', include('team_dashboard.urls')),        # مسار الفريق
+    # path('site-admin/', include('public_site.admin_urls')),  # لوحة تحكم الموقع (عربي فقط)
+    path('', include('public_site.urls')),                     # صفحات الموقع
+]
+
+# دعم ملفات الميديا
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# معالجات الأخطاء
+handler403 = 'team_dashboard.views.custom_403'
+handler404 = 'team_dashboard.views.custom_404'
+handler500 = 'team_dashboard.views.custom_500'
+
