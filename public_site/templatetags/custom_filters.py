e443022b8ea3e89be2360b71deb34a515cffd0bc
Seyf Eddine <84080839+seyf-eddine19@users.noreply.github.com>
2025-07-12 13:08:36 +0100

    Add files via upload

--- public_site/templatetags/custom_filters.py
+++ public_site/templatetags/custom_filters.py
@@ -3,6 +3,22 @@ from django import template
 register = template.Library()
 
 @register.filter
+def get_by_key(queryset, key):
+    try:
+        for obj in queryset:
+            if obj.key == key:
+                return obj
+    except Exception:
+        pass
+    return None
+
+@register.filter
+def get_content_by_lang(content_obj, lang):
+    if content_obj:
+        return content_obj.content_ar if lang == 'ar' else content_obj.content_en
+    return ''
+
+@register.filter
 def get_text(content_list, key):
     if not content_list:
         return ''
